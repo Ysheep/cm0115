82568d5f09d6ec58622525cf0819b66c6272af35
Ysheep <mianjun8616@163.com>
2017-01-15 20:32:35 +0800

    github update 02
    github update 02

--- src/cn/tarena/github/test/Test01.java
+++ src/cn/tarena/github/test/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("Hello github EGit");
 		System.out.println("ok, github EGit return");
+		System.out.println("github update return02");
 	}
 }
